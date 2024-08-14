c9a160e0613280f42c666c7c7f34c07b820cd982
MishaITSTEP <mishakravchuk2004@gmail.com>
2024-08-14 19:40:18 +0300

    a3

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    private static final String URL="postgresql://ep-shrill-resonance-a2ewhu7l.eu-central-1.aws.neon.tech/jjj?sslmode=require";
+    private static final String USER="jjj_owner";
+    private static final String PASSWORD="TQi2LaRf5uzl";
+
+    public static void main(String[] args) {
+
+    }
+}
